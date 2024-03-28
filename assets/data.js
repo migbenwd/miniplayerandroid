3665a4f0566de259434fe91b3518bb48c7ec7e7a
migben.com <miguelangel.benitezyanez@gmail.com>
2024-03-28 19:46:36 -0400

    cambios en local

---
+++ assets/data.js
@@ -0,0 +1,21 @@
+const podcasts = [
+    
+  {
+    title: 'Italiano',
+    artist: 'Serafino',
+    // artwork: require('./img/2142935_0_idF_idE_500x500.jpg'),
+    url: 'https://api.spreaker.com/download/episode/43447400/letteraoreste.mp3',
+  },
+
+  {
+    title: 'Emisora 2',
+    artist: 'Altavoz Radio',
+    // artwork: require('./img/2142935_0_idF_idE_500x500.jpg'),
+    url: 'https://streaming.shoutcast.com/radio-65',
+  },
+
+
+
+  ];
+  
+  export default podcasts;
